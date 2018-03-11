5cc1cf16f2ef987f99d5e0f33a858ecab168093a
Ashleigh Adams <ashleighadams@Ashleighs-MacBook-Air.local>
2018-03-11 16:35:51 -0600

    fixed .vimrc spacing issue for github and adding comments

--- sumsetProgram.cpp
+++ sumsetProgram.cpp
@@ -14,11 +14,11 @@ int main(void) {
     int d = 0;
     int h = 0;
 
-    getUserValues(n,d);
+    getUserValues(n,d); //gets user values for calculations
 
     int total = totalCombinations(d,n); // ( n choose k ) returns the total number of possible unique combinations
 
-	//INITIALIZE ARRAY TO HOLD ALL UNIQUE COMBINATIONS OF Z_n
+    //INITIALIZE ARRAY TO HOLD ALL UNIQUE COMBINATIONS OF Z_n: 2d: nxd 
     int** allCombos = new int*[total];
     for(int i=0; i<total; ++i) {
         allCombos[i] = new int[d];
@@ -63,56 +63,57 @@ int totalCombinations(int& k, int& n) {
 
 void allSubsets(int runs, int s, int d, int n, int **allCombos, int total){
 
-    bool midLoopNotComplete = true;
+    bool midLoopNotComplete = true; //
 
-	if (total == 0) {
+	if (total == 0) { //base case when total number of combinations have been found, function returns
 		return;
 	}
 
 	do {
-		int count = d-1;
-		for(int iterations = n-d+1-s; iterations > 0 && count < n; iterations--) {
-		    total--;
+        int count = d-1; //for last line increments, it will always begin at d-1
+        for(int iterations = n-d+1-s; iterations > 0 && count < n; iterations--) {
+		    total--; //decrements the total number of combinations
 
-			for(int i=0; i<d; i++){	
+        for(int i=0; i<d; i++){	
 
-				allCombos[total][i] = runs;
+            allCombos[total][i] = runs;
 
-			    if (i >= 1 && i < d-1) {
-				    allCombos[total][i] = i+s;
-				}
+            if (i >= 1 && i < d-1) {
+                allCombos[total][i] = i+s;
+            }
 
-				if (i == d-1) {
-			        while (count <= allCombos[total][i-1]) {
-				        count++;
-				    }
-				    allCombos[total][i] = count;
-			    }
+            if (i == d-1) {
+                while (count <= allCombos[total][i-1]) {
+                    count++;
+                }
+                allCombos[total][i] = count;
+            }
 
-		   } // SMALL LOOP
+        } // SMALL LOOP
 
-            printCombos(allCombos, total, d);
-			std::cout << std::endl;
+            printCombos(allCombos, total, d); //prints all combinations found
+            std::cout << std::endl;
 
             count++;
         } // BIGGER LOOP
 
-	    s++;	
+	    s++; //increments addition element for midloop increments	
 
+		
         if (allCombos[total][d-1] == n-1 &&  allCombos[total][d-2] == n-2 ) {
-            midLoopNotComplete = false;
+            midLoopNotComplete = false; //When the final two elements in any combination of length d are n-1 and n-2, the mid loop is now complete
         }
 
     }while( midLoopNotComplete ); //MID LOOP
 
-	runs++;
+    runs++;
     allSubsets(runs, runs, d, n, allCombos, total); //MAIN LOOP
 
     return;
 }
 
 
-
+//PRINTS COMBINATIONS: { a b .... z }
 void printCombos(int **allCombos, int total, int d) {
 
     std::cout << "{ ";
